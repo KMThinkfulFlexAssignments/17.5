7d62bb125d727c5e023f08639b64696a7130e634
Kevin Moore <moore.kevin.p@gmail.com>
2020-03-16 13:22:21 -0400

    finished test suite, all tests passing

--- playstoreServer/test/app.test.js
+++ playstoreServer/test/app.test.js
@@ -4,25 +4,27 @@ const supertest = require('supertest');
 const app = require('../app');
 
 describe('GET /apps endpoint', () => {
-  it('should return an object describing an app', () => {
+  it('should return an array of objects describing an app', () => {
     return supertest(app)
-      .get('/apps?genres=card')
+      .get('/apps')
       .query({'Genres': 'Card'})
       .expect(200)
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .expect('Content-Type', 'application/json; charset=utf-8')
       .then(res => {
-        expect(res.body).to.be.an('object');
+        expect(res.body).to.be.an('array');
       });
   });
   
   it('should contain Genres and/or Rating keys', () => {
     return supertest(app)
-      .get('/apps?genres=card')
+      .get('/apps')
       .query({'Genres': 'Card', 'Rating': 4})
       .expect(200)
       .then(res => {
         res.body.forEach(app => {
-          expect(res.body).to.have.any.keys('Genres', 'Rating');
+          expect(res.body).to.have.lengthOf.at.least(1);
+          const result = res.body[0];
+          expect(result).to.include.all.keys('Genres', 'Rating');
         });
       });
   });
